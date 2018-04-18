9d3b2ab6233b9ced4b603df3c3ed6ba8712b36cb
AadityaSapate <35969231+AadityaSapate@users.noreply.github.com>
2018-04-18 12:51:29 +0530

    Update inConfig.php

--- linkedin login/inConfig.php
+++ linkedin login/inConfig.php
@@ -6,8 +6,8 @@ require_once 'src/oauth_client.php';
 /*
  * Configuration and setup LinkedIn API
  */
-$apiKey = '8119ruk86qvrqi';
-$apiSecret = 'ymr1zaNWZaxvqFcs';
-$redirectURL = 'http://ec2-18-217-131-163.us-east-2.compute.amazonaws.com/linkedin/';
+$apiKey = '';
+$apiSecret = '';
+$redirectURL = '';
 $scope = 'r_basicprofile r_emailaddress'; //API permissions
 ?>
