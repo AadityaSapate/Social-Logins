3ea47bb749ecca8c8c34d98d04a7054f734d986a
AadityaSapate <35969231+AadityaSapate@users.noreply.github.com>
2018-04-18 12:53:59 +0530

    Update twConfig.php

--- twitter login/twConfig.php
+++ twitter login/twConfig.php
@@ -9,8 +9,8 @@ include_once 'src/twitteroauth.php';
 /*
  * Configuration and setup Twitter API
  */
-$consumerKey = '9YkpjnKmwpigvCDg7fDpdXi4i';
-$consumerSecret = 'ibuEIKdrr5PS6H24zl7ZaeR72GgeVEhvoWneBmekGlzsmfRu0X';
-$redirectURL = 'http://18.217.131.163/twitter/';
+$consumerKey = '';
+$consumerSecret = '';
+$redirectURL = '';
 
 ?>
